319cedac03f8bb3aaf796ee82ac33a9c39bfd8fe
H4ck3rM4n <apavalac@student.42.fr>
2019-07-26 14:36:00 +0300

    finishde get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,52 +1,59 @@
-#include "libft.h"
-#include "stdio.h"
+#include "get_next_line.h"
 
-#define BUFF_SIZE 64
-
-int     get_next_line(int fd, char **line)
+static  t_list  *get_file(int fd, t_list **files)
 {
-    size_t  alloc_length = BUFF_SIZE;
-    size_t  data_length = 0;
-    char    *data = malloc(alloc_length);
-    char    c;
+    t_list  *found;
 
-    while(1)
+    found = *files;
+    while(found != NULL)
     {
-        read(fd, &c, 1);
-        data[data_length] = c;
-        if(data[data_length] == '\n' || data[data_length] == EOF)
-            break;
-        data_length++;
-        if(data_length == alloc_length)
-        {
-            size_t new_length = alloc_length << 1;
-            data = realloc(data, new_length);
-            if(!data)
-                return (-1);
-            alloc_length = new_length;
-        }
+        if((int)found->content_size == fd)
+            return(found);
+        found = found->next;
     }
-    if(data[data_length] == '\n' || data[data_length] == EOF)
-        data[data_length] = '\0';
-    
-    data = realloc(data, data_length);
-    //ft_memcpy(*line, data, data_length);
-    *line = data;
-    //free(data);
-    return (0);
+
+    ft_lstadd(files, ft_lstnew("\0", fd));
+    return (*files);
 }
 
-int     main(int ac, char **av)
+int             get_next_line(int fd, char **line)
 {
-    int fd = open(av[1], O_RDONLY);
-    char *line;
+    static  t_list  *files;
+    size_t  byte_read;
+    size_t  offset;
+    char    *buffer;
 
-    int n = 10;
-    while(n--)
+    buffer = (char*)malloc(BUFF_SIZE + 1);
+    t_list  *file;
+
+    file = get_file(fd, &files);
+    if(!file)
+        return (-1);
+
+    while((byte_read = read(fd, buffer, BUFF_SIZE)) == BUFF_SIZE)
     {
-        get_next_line(fd, &line);
-        printf("%s\n", line);
-        //free(line);
+        buffer[byte_read] = '\0';
+        file->content = ft_strjoin(file->content, buffer);
+    }
+    
+    buffer[byte_read] = '\0';
+    file->content = ft_strjoin(file->content, buffer);
+    
+    int i = 0;
+    while((((char*)(file->content))[i] != '\n' && ((char*)(file->content))[i] != EOF) && i < ft_strlen(file->content))
+        i++;
+
+    *line = ft_strsub(file->content, 0, i);
+    
+    if(ft_strchr(file->content, '\n'))
+    {
+        ft_strcpy(file->content, ft_strchr(file->content, '\n') + 1);
+        return (1);
+    }
+    else
+    {
+        ft_strcpy(file->content, ft_strsub(file->content, 0, ft_strlen(file->content)));
+        return(0);
     }
 
     return (0);
